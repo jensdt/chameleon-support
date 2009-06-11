40fd264c7f9f0fa7331f3a5b577d86d6d02c3037
marko <marko@objectron.dyn.cs.kuleuven.be>
2009-06-11 17:56:12 +0200

    init

---
+++ src/chameleon/support/type/EmptyTypeElement.java
@@ -0,0 +1,32 @@
+package chameleon.support.type;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import chameleon.core.element.Element;
+import chameleon.core.member.Member;
+import chameleon.core.type.Type;
+import chameleon.core.type.TypeElementImpl;
+
+public class EmptyTypeElement extends TypeElementImpl<EmptyTypeElement,Type> {
+
+	@Override
+	public EmptyTypeElement clone() {
+		return new EmptyTypeElement();
+	}
+
+	public Set<Member> getIntroducedMembers() {
+		return new HashSet<Member>();
+	}
+
+	public Type getNearestType() {
+		return parent().getNearestType();
+	}
+
+	public List<Element> children() {
+		return new ArrayList<Element>();
+	}
+
+}
