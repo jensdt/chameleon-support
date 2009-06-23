849f05e303d2d5afd74c2765947ff498917bdd0c
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-06-23 13:33:19 +0200

    added lookup for enum label

--- src/chameleon/support/statement/EnumLabel.java
+++ src/chameleon/support/statement/EnumLabel.java
@@ -3,9 +3,17 @@ package chameleon.support.statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import chameleon.core.MetamodelException;
+import chameleon.core.declaration.Declaration;
+import chameleon.core.declaration.DeclarationSelector;
+import chameleon.core.declaration.SimpleNameSignature;
 import chameleon.core.element.Element;
+import chameleon.core.expression.Expression;
+import chameleon.core.expression.SelectorWithoutOrder;
+import chameleon.core.reference.CrossReference;
+import chameleon.core.variable.Variable;
 
-public class EnumLabel extends SwitchLabel<EnumLabel>{
+public class EnumLabel extends SwitchLabel<EnumLabel> implements CrossReference<EnumLabel, SwitchCase>{
 
 	public EnumLabel(String name) {
 		_name = name;
@@ -29,5 +37,15 @@ public class EnumLabel extends SwitchLabel<EnumLabel>{
 	}
 	
 	private String _name;
+
+	public Declaration getElement() throws MetamodelException {
+		// class must move to Jnome because of enum dependency?
+		Expression switchExpr = parent().parent().getExpression();
+		return switchExpr.getType().targetContext().lookUp(selector());
+	}
+
+	private DeclarationSelector selector() {
+		return new SelectorWithoutOrder<Variable>(new SimpleNameSignature(name()),Variable.class);
+	}
 	
 }
