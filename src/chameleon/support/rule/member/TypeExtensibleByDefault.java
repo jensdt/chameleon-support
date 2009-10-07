82d927831702e6276269d128cef301a593725711
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-10-07 14:24:51 +0200

    init

---
+++ src/chameleon/support/rule/member/TypeExtensibleByDefault.java
@@ -0,0 +1,23 @@
+package chameleon.support.rule.member;
+
+import org.rejuse.property.PropertySet;
+
+import chameleon.core.element.Element;
+import chameleon.core.member.Member;
+import chameleon.core.property.ChameleonProperty;
+import chameleon.core.property.PropertyRule;
+import chameleon.oo.language.ObjectOrientedLanguage;
+
+public class TypeExtensibleByDefault extends PropertyRule {
+
+	@Override
+	public boolean appliesTo(Element element) {
+		return element instanceof Member;
+	}
+
+	@Override
+	public PropertySet<Element, ChameleonProperty> suggestedProperties(Element element) {
+		return createSet(language(ObjectOrientedLanguage.class).EXTENSIBLE);
+	}
+
+}
