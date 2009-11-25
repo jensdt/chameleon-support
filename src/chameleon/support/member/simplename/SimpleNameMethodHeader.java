8447bd182c9618ec9141045c82aca97354213c06
marko <marko@objectron.dyn.cs.kuleuven.be>
2009-11-25 16:06:10 +0100

    loosened parametric constraint on method header

--- src/chameleon/support/member/simplename/SimpleNameMethodHeader.java
+++ src/chameleon/support/member/simplename/SimpleNameMethodHeader.java
@@ -1,21 +1,13 @@
 package chameleon.support.member.simplename;
 
-import java.util.List;
-
-import chameleon.core.compilationunit.CompilationUnit;
-import chameleon.core.declaration.Declaration;
-import chameleon.core.lookup.DeclarationSelector;
-import chameleon.core.lookup.LookupException;
-import chameleon.core.method.Method;
 import chameleon.core.method.MethodHeader;
-import chameleon.core.namespace.Namespace;
-import chameleon.core.namespacepart.NamespacePart;
+import chameleon.core.namespace.NamespaceElement;
 import chameleon.core.validation.BasicProblem;
 import chameleon.core.validation.Valid;
 import chameleon.core.validation.VerificationResult;
 import chameleon.core.variable.FormalParameter;
 
-public class SimpleNameMethodHeader<E extends SimpleNameMethodHeader, P extends Method, S extends SimpleNameMethodSignature> extends MethodHeader<E, P, S>{
+public class SimpleNameMethodHeader<E extends SimpleNameMethodHeader, P extends NamespaceElement, S extends SimpleNameMethodSignature> extends MethodHeader<E, P, S>{
 
   public SimpleNameMethodHeader(String name) {
     setName(name);
@@ -38,7 +30,7 @@ public class SimpleNameMethodHeader<E extends SimpleNameMethodHeader, P extends 
 
 	@Override
 	public S signature() {
-		SimpleNameMethodSignature result =  new SimpleNameMethodSignature(getName());
+		SimpleNameMethodSignature result = new SimpleNameMethodSignature(getName());
 		result.setUniParent(parent());
 		for(FormalParameter param: formalParameters()) {
 			result.add(param.getTypeReference().clone());
